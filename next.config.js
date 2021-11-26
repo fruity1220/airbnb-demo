21622c14cc05b523625d33b5ac8e8d629be7274f
fruity1220 <rachelseo1220@gmail.com>
2021-11-26 19:10:56 +0900

    'dayfour'

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@ module.exports = {
     images: {
         domains: ['links.papareact.com'],
     },
+    env: {
+        mapbox_key: 'pk.eyJ1IjoiYmFieXNlbzEyMjAiLCJhIjoiY2t3ZzVuN3VoMGxlZjJ2cXFjeG5rMW11MiJ9.ktY9oAQlNF_HXZHj3i3j1g'
+    }
 };
+
+//SHOULD NOT INCLUDE ANY PRIVATE KEY
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import 'tailwindcss/tailwind.css';
-import '../styles/global.css';
-import ProgressBar from "@badrap/bar-of-progress";
+import "../styles/global.css";
 import Router from "next/router";
+
+import ProgressBar from "@badrap/bar-of-progress";
 
 const progress = new ProgressBar({
   size: 4,
-  color:"#FE595E",
-  className ="z-50",
+  color: "#FE595E",
+  //className = "z-50",
   delay: 100,
 });
 
